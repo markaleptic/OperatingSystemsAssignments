8a38cbc93042a8c2a3f14aed071be0c148169a0e
Mark Allred <mark@Marks-MacBook-Pro.local>
2019-02-27 13:47:06 -0700

    Reconstructed constructors and started runnable interface

--- Assign4/src/ParallelPi.java
+++ Assign4/src/ParallelPi.java
@@ -6,32 +6,16 @@ import java.util.Collections;
 import java.lang.Runtime;
 
 
-public class ParallelPi{
-    private int numDigits = 1000;
+public class ParallelPi implements Runnable{
+    private String threadName;
     private Queue <Integer> TaskQueue;
     private HashMap <Integer,Integer> ResultTable;
-    private int processors = 1;
 
 
-
-    public ParallelPi(int digitPrecision){
-        if(digitPrecision != 1000){
-            this.numDigits = digitPrecision;
-        }
-        
-        // Create threads equal to the number of available processors
-        processors = Runtime.getRuntime().availableProcessors();
-        
-        // Create randomized task queue by generating a range from 1 to numDigits, 
-        // construct TaskQueue with randomized range. Used List and ArrayList to
-        // work easily with Collections.shuffle().
-        ArrayList<Integer> rng = new ArrayList<Integer>(this.numDigits);
-        for(int i = 1; i < this.numDigits; i++){
-            rng.add(i);
-        }
-        Collections.shuffle(rng);
-        this.TaskQueue = new LinkedList(rng);
-        this.ResultTable = new HashMap<Integer,Integer>(this.numDigits);
+    public ParallelPi(String name, Queue sharedTaskQueue, HashMap sharedResultTable){
+        this.threadName = name;
+        this.TaskQueue = sharedTaskQueue;
+        this.ResultTable = sharedResultTable;
     }
 
 
